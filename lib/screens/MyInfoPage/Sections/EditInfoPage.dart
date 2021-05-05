38f264de7e62014202bc246130b4ecf57cef6a7c
ByeongWooCho <whquddn55@gmail.com>
2021-05-05 23:51:45 +0900

    Modify - EditInfoPage, EditPage EditInfoPage: added receive editpage's return value EditPage: modify more abstractively

--- lib/screens/MyInfoPage/Sections/EditInfoPage.dart
+++ lib/screens/MyInfoPage/Sections/EditInfoPage.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'ListElement.dart';
 import 'EditPage.dart';
 
+class EditPageArguments {
+  final String title;
+  final String message;
+
+  EditPageArguments(this.title, this.message);
+}
+
 class EditInfoPage extends StatelessWidget {
   static const id = "editInfo_Page";
   @override
@@ -30,14 +37,35 @@ class EditInfoPage extends StatelessWidget {
           ),
         ),
         Divider(thickness: 10,),
-        ListElement(title: "이름", value: "홍길동", onTap: () { Navigator.pushNamed(context, EditPage.id); }), // dummy
+        ListElement(title: "이름", value: "홍길동", onTap: () { _navigateAndDisplaySelection(context, EditPageArguments(
+          '이름 변경',
+          '이름'
+        )); }), // dummy
         Divider(thickness: 1,),
-        ListElement(title: "이메일", value: "whquddn55@gmail.com"), // dummy
+        ListElement(title: "이메일", value: "whquddn55@gmail.com", onTap: () { _navigateAndDisplaySelection(context, EditPageArguments(
+          '이메일 변경',
+          '이메일'
+        )); }), // dummy
         Divider(thickness: 1,),
-        ListElement(title: "전화번호", value: "01077087809"), // dummy
+        ListElement(title: "전화번호", value: "01077087809", onTap: () { _navigateAndDisplaySelection(context, EditPageArguments(
+          '전화번호 변경',
+          '전화번호'
+        )); }), // dummy
+        Divider(thickness: 1,),
+        ListElement(title: "비밀번호", onTap: () { _navigateAndDisplaySelection(context, EditPageArguments(
+          '비밀번호 변경',
+          '비밀번호'
+        )); }), // dummy
         Divider(thickness: 1,)
       ],
       ),
     );
+  }
+
+  _navigateAndDisplaySelection(BuildContext context, EditPageArguments arguments) async {
+    final result = await Navigator.pushNamed(context, EditPage.id, arguments: arguments);
+    // 
+    ScaffoldMessenger.of(context).removeCurrentSnackBar();
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(result.toString())));
   }
 }
--- lib/screens/MyInfoPage/Sections/EditPage.dart
+++ lib/screens/MyInfoPage/Sections/EditPage.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:what_eat/screens/MyInfoPage/Sections/EditInfoPage.dart';
 import 'ListElement.dart';
 
 class EditPage extends StatelessWidget {
   static const id = "edit_Page";
   @override
   Widget build(BuildContext context) {
+    final EditPageArguments args = ModalRoute.of(context).settings.arguments;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('내 정보 수정'),
+        title: Text(args.title),
         leading: IconButton(
           onPressed: () {
-            Navigator.pop(context);
+            Navigator.pop(context, false);
           }, 
           icon: Icon(Icons.arrow_back),
         ),
       ),
-      body: Column( children: [
-      ]
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: args.message,
+              ),
+              obscureText: (args.message.compareTo("비밀 번호") == 0 ? true : false),
+            )
+          ),
+          ElevatedButton(
+            child: Text('확인'),
+            onPressed: () {
+              // Todo: 서버에 정보수정 요청
+              Navigator.pop(context, true); // 정보수정이 됐으면 true 반환 else 재수정 요청
+            },
+          )
+        ],
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
       ),
     );
   }
